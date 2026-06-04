5a2448c1a58ea5289c26f12a962ecad667ac042e
terdeck <taeraesuu@gmail.com>
2026-06-03 22:30:07 -0400

    made homehome the home pg for app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeHome } from './home-home/home-home';
+import { ServicesHome } from './services-home/services-home';
+import { ContactHome } from './contact-home/contact-home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    
+];
