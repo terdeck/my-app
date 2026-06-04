7a5b5544e9a9778792e28c92a3374b0383939526
terdeck <taeraesuu@gmail.com>
2026-06-03 22:35:28 -0400

    finished updating routes pg to redirect to homehome

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,5 +4,9 @@ import { ServicesHome } from './services-home/services-home';
 import { ContactHome } from './contact-home/contact-home';
 
 export const routes: Routes = [
-    
+    { path: 'home', component: HomeHome },
+    { path: 'services', component: ServicesHome },
+    { path: 'projects', component: ContactHome },
+    { path: 'contact', component: ContactHome },
+    { path: '', redirectTo:'/home', pathMatch:'full' }
 ];
