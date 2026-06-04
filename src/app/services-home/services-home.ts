be028455c5569e90bc3745b1c4efc771ea243f99
terdeck <taeraesuu@gmail.com>
2026-06-04 14:42:38 -0400

    added export for OnInit implement

--- src/app/services-home/services-home.ts
+++ src/app/services-home/services-home.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HighlightDirective } from './highlight';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-services-home',
   standalone: true,
-  imports: [HighlightDirective],
+  imports: [HighlightDirective, NgFor],
   templateUrl: './services-home.html',
   styleUrl: './services-home.css',
 })
-export class ServicesHomeComponent {
+export class ServicesHomeComponent implements OnInit {
+  bankingServices = ['Issue business loans',
+   'Open new checking and/or saving accounts',
+   'Help clients with account-related issues such as transfers, withdrawals, and deposits',
+   'Provide periodic reports and analysis',
+  'Monitor client accounts and keep information up to date',
+  'Guide clients through their savings, loan, mortgage, and retirement options',
+   'Refer customers to loan officers or other financial specialists',
+  'Resolve debit and credit issues'];
 
+  ngOnInit(): void { }
 }
