5efa415a72f99a2fb6d3d4dfd82780d69b7b866c
terdeck <taeraesuu@gmail.com>
2026-06-04 13:39:06 -0400

    interpolated data w/ home-home

--- src/app/home-home/home-home.ts
+++ src/app/home-home/home-home.ts
@@ -1,9 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home-home',
+  standalone: true,
   imports: [],
   templateUrl: './home-home.html',
   styleUrl: './home-home.css',
 })
-export class HomeHome {}
+export class HomeHome implements OnInit {
+   bankerName = "Elizabeth Franklin"; 
+  bankerAddress = '123 Main Street'; 
+  bankerCity = 'Hoboken'; 
+  bankerState = 'NJ'; 
+  bankerZip = '07030'; 
+  bankerPhone = '(201) 748-6000'; 
+  bankerEmail = 'efranklin@email.com'; 
+  summary = `Personable and organized Banker with 6+ years of experience matching customers with suitable account 
+             solutions and consistently surpassing quotas for new checking, savings, and credit card accounts.`;
+  school = 'University of Chicago'; 
+  schoolDates = 'September 2009 - December 2013'; 
+  schoolDegree = 'Bachelor of Arts, Finance'; 
+  employerName = 'Tradelot'; 
+  jobTitle = 'Banker'; 
+  employmentDates = 'January 2014 - Present'; 
+  
+  constructor() { }
+  ngOnInit(): void {   }
+}
