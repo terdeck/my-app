6f0261b8997a87739bc634831bf1ed7369232482
terdeck <taeraesuu@gmail.com>
2026-06-04 14:07:18 -0400

    built business card form and styling

--- src/app/projects-home/projects-home.ts
+++ src/app/projects-home/projects-home.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-projects-home',
@@ -6,4 +6,31 @@ import { Component } from '@angular/core';
   templateUrl: './projects-home.html',
   styleUrl: './projects-home.css',
 })
-export class ProjectsHome {}
+export class ProjectsHome implements OnInit { 
+  firstName: string ="William"; 
+  lastName: string = "Wallace"; 
+  phoneNumber: string = "(201) 748-6000";
+
+  setProperty(event: Event){ 
+   let target = event.target as HTMLInputElement 
+   let property: string = target.id 
+   switch(property){ 
+     case "firstNameInput": 
+       this.firstName = target.value; 
+       break; 
+     case "lastNameInput": 
+       this.lastName = target.value; 
+       break; 
+     case "phoneNumberInput": 
+       this.phoneNumber = target.value; 
+       break; 
+     default: 
+        console.warn("No matching ID found!") 
+       break; 
+    } 
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {  }
+}
