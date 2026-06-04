ea94d1719f95040b0c10749c9d6150a8de5d8fd8
terdeck <taeraesuu@gmail.com>
2026-06-03 21:36:20 -0400

    deleted generic html filler, added project info, and created nav menu with home, services, project, and contact

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgClass } from "../../node_modules/@angular/common/types/_common_module-chunk";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgClass],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
