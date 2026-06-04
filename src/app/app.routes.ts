cbbb8d6ccb49a013e52029af4962858e514facaa
terdeck <taeraesuu@gmail.com>
2026-06-04 13:28:24 -0400

    playing with app.routes, was having an issue with just serviceshome import

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeHome } from './home-home/home-home';
-import { ServicesHome } from './services-home/services-home';
+import { ServicesHomeComponent } from './services-home/services-home';
 import { ContactHome } from './contact-home/contact-home';
 
 export const routes: Routes = [
     { path: 'home', component: HomeHome },
-    { path: 'services', component: ServicesHome },
+    { path: 'services', component: ServicesHomeComponent },
     { path: 'projects', component: ContactHome },
     { path: 'contact', component: ContactHome },
     { path: '', redirectTo:'/home', pathMatch:'full' }
